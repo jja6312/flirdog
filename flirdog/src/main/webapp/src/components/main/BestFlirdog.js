0c7cf23fcc89c9d8da7822ee97699f0102fc4c08
jja6312 <127708710+jja6312@users.noreply.github.com>
2023-11-04 22:37:16 +0900

    스타일:메인페이지 강아지소모임 스타일 구현

--- flirdog/src/main/webapp/src/components/main/BestFlirdog.js
+++ flirdog/src/main/webapp/src/components/main/BestFlirdog.js
@@ -5,7 +5,7 @@ import NavigateBtn from "./NavigateBtn";
 const BestFlirdog = () => {
   return (
     <>
-      <div className="row mt-7 mx-2">
+      <div className="row mt-10 mx-2">
         <div className="col-12">
           <img
             className="bestFlirdogTitle"
@@ -13,7 +13,7 @@ const BestFlirdog = () => {
           />
         </div>
       </div>
-      <div className="row mt-5">
+      <div className="row mt-8">
         <div
           className="col-3 d-flex justify-content-center align-items-center
         rankingBtn pinkDivColor "
@@ -27,13 +27,19 @@ const BestFlirdog = () => {
           <span>지역 랭킹</span>
         </div>
       </div>
-      <div className="row mt-5 d-flex justify-content-between">
+      <div className="row mt-8 d-flex justify-content-between">
         <BestFlirdogImg srcImg="/image/main/dog1.jpg" text="999" />
         <BestFlirdogImg srcImg="/image/main/dog2.jpg" text="784" />
         <BestFlirdogImg srcImg="/image/main/dog3.jpg" text="654" />
       </div>
-      <div className="navigateBtnContainer mt-5">
-        <NavigateBtn text="등록하러 가기" url="/"></NavigateBtn>
+      <div className="navigateBtnContainer mt-8">
+        <NavigateBtn
+          text="등록하러 가기"
+          url="/"
+          fontSize="2.5vw"
+          btnWidth="40vw"
+          btnHeight="12vh"
+        ></NavigateBtn>
       </div>
     </>
   );
--- flirdog/src/main/webapp/src/components/main/NavigateBtn.js
+++ flirdog/src/main/webapp/src/components/main/NavigateBtn.js
@@ -2,11 +2,21 @@ import React from "react";
 import "../../css/main/NavigateBtn.css";
 import { Link } from "react-router-dom";
 
-const NavigateBtn = ({ text, url }) => {
+const NavigateBtn = ({ text, url, fontSize, btnWidth, btnHeight }) => {
   return (
     <>
-      <Link to={url} style={{ textDecoration: "none" }}>
-        <div className="navigateBtn">{text}</div>
+      <Link
+        to={url}
+        style={{
+          textDecoration: "none",
+        }}
+      >
+        <div
+          className="navigateBtn"
+          style={{ fontSize: fontSize, width: btnWidth, height: btnHeight }}
+        >
+          {text}
+        </div>
       </Link>
     </>
   );
