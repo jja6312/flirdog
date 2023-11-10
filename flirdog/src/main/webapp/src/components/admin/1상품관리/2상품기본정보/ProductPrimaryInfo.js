ea3da9e8cac6ba66b690f04480bf2ba0cd350e7f
lavi15 <biz.jongin@gmail.com>
2023-11-10 12:45:14 +0900

    스프링 설정 최종 완료

--- flirdog/src/main/webapp/src/components/admin/1상품관리/2상품기본정보/ProductPrimaryInfo.js
+++ flirdog/src/main/webapp/src/components/admin/1상품관리/2상품기본정보/ProductPrimaryInfo.js
@@ -1,5 +1,7 @@
 import React from "react";
-import styles from "../../../../css/admin/2상품기본정보/productPrimaryInfo.module.css";
+import styles from "../../../../css/admin/1상품관리/productPrimaryInfo.module.css";
+import InputGroup from "react-bootstrap/InputGroup";
+import Form from "react-bootstrap/Form";
 
 const ProductPrimaryInfo = () => {
   return (
@@ -7,19 +9,49 @@ const ProductPrimaryInfo = () => {
       <table className={styles.tableContainer}>
         <tr>
           <th className={styles.tableTh}>브랜드</th>
-          <td className={styles.tableTd}>d</td>
+          <td className={styles.tableTd}>
+            <InputGroup size="sm">
+              <Form.Control
+                aria-label="Large"
+                aria-describedby="inputGroup-sizing-sm"
+              />
+            </InputGroup>
+          </td>
         </tr>
         <tr>
           <th className={styles.tableTh}>상품명</th>
-          <td className={styles.tableTd}></td>
+          <td className={styles.tableTd}>
+            <InputGroup size="sm">
+              <Form.Control
+                aria-label="Large"
+                aria-describedby="inputGroup-sizing-sm"
+              />
+            </InputGroup>
+          </td>
         </tr>
         <tr>
           <th className={styles.tableTh}>상품 간단 설명</th>
-          <td className={styles.tableTd}></td>
+          <td className={styles.tableTd}>
+            <InputGroup size="sm">
+              <Form.Control
+                aria-label="Large"
+                aria-describedby="inputGroup-sizing-sm"
+              />
+            </InputGroup>
+          </td>
         </tr>
         <tr>
           <th className={styles.tableTh}>출시일</th>
-          <td className={styles.tableTd}></td>
+          <td className={styles.tableTd}>
+            <input
+              style={{ color: "gray", fontSize: "0.8rem" }}
+              className="mx-2"
+              type="date"
+              id="releaseDate"
+              name="releaseDate"
+              required
+            />
+          </td>
         </tr>
       </table>
     </div>
