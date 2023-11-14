44b110a7d09eb68277ce59605710eef8343585c8
jja6312 <127708710+jja6312@users.noreply.github.com>
2023-11-14 14:30:13 +0900

    스타일:소모임디테일탭 초기설정

---
+++ flirdog/src/main/webapp/src/components/somoim/detail/SomoimDetailMain.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SomoimDetailMain = () => {
+    return (
+        <div>
+            소모임 메인 페이지 입니다.
+        </div>
+    );
+};
+
+export default SomoimDetailMain;
