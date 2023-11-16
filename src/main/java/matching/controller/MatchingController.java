e5beed0553e4d406eb969611b937388fa3077af9
Kim-HyeonSeong <kimhs1258@naver.com>
2023-11-16 19:58:01 +0900

    백엔드 : 컨트롤러 설정

--- src/main/java/matching/controller/MatchingController.java
+++ src/main/java/matching/controller/MatchingController.java
@@ -1,4 +1,29 @@
 package matching.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping(path = "date")
 public class MatchingController {
+	
+	@GetMapping("list")
+	public String dateList() {
+		return "/date/dateList";
+	}
+	@GetMapping("Write")
+	public String dateWrite() {
+		return "/date/dateWrite";
+	}
+	@GetMapping("dateReadMore")
+	public String dateReadMore() {
+		return "/date/dateReadMore";
+	}
+	@GetMapping("update")
+	public String dateUpdate() {
+		return "/date/dateUpdate";
+	}
 }
