33251f391ac0f61dc4d01da27ef747d2ee45350c
Manchan777 <freshguni@gmail.com>
2023-11-16 18:23:21 +0900

    소모임 1116 젠킨스 에러 반영

--- src/main/webapp/src/components/somoim/detail/SomoimDetail.js
+++ src/main/webapp/src/components/somoim/detail/SomoimDetail.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import React from 'react';
+import { useParams } from 'react-router-dom';
 import { Button, Container } from 'react-bootstrap';
 import Header from '../../main/Header';
 import Footer from '../../main/Footer';
