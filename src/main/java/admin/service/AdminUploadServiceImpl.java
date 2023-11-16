81d048db710ef42d747b648a003b30a6eac9983e
jja6312 <127708710+jja6312@users.noreply.github.com>
2023-11-16 15:48:33 +0900

    제발설정

--- src/main/java/admin/service/AdminUploadServiceImpl.java
+++ src/main/java/admin/service/AdminUploadServiceImpl.java
@@ -2,22 +2,20 @@ package admin.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import product.bean.Product;
-import product.repository.ProductRepository;
 
 @Service
 @Transactional
 public class AdminUploadServiceImpl implements AdminUploadService {
-	@Autowired
-	private ProductRepository productRepository;
+	// @Autowired
+	// private ProductRepository productRepository;
 
 	@Override
 	public void upload(List<Product> userImageList) {
-		productRepository.saveAll(userImageList);
+		// productRepository.saveAll(userImageList);
 
 	}
 
