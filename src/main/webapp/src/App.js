e638dc32454eb90fc0e2078c7dbbfb10a49345d9
Kim-HyeonSeong <kimhs1258@naver.com>
2023-11-17 14:40:42 +0900

    스타일:1117합침

--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Main from "./components/main/Main";
-import DateList from "./components/date/DateList";
+
 
 import "./css/reset.css";
 import Test from "./components/user/Test";
+import DateList from "./components/date/DateList";
 import DateWrite from "./components/date/DateWrite";
+import DateReadMore from "./components/date/DateReadMore";
+import DateUpdate from "./components/date/DateUpdate";
 
 import Admin from "./components/admin/Admin";
 import ProductUploadForm from "./components/admin/1상품관리/ProductUploadForm";
@@ -61,16 +64,14 @@ const App = () => {
                     <Route path="/mypage/MyarticleRounge" element={<MyarticleRounge/>}/>
                     {/* 김찬영마이페이지//================================================================= */}
 
+                    {/* 매칭페이지 */}
                     <Route path="/" element={<Main></Main>}/>
                     <Route path="/user/userTest" element={<Test/>}/>
                     <Route path="/date/dateList" element={<DateList/>}/>
                     <Route path="/date/dateWrite" element={<DateWrite/>}/>
-                    <Route path="/admin" element={<Admin/>}/>
+                    <Route path="/date/dateUpdate" element={<DateUpdate />}/>
+                    <Route path="/date/dateReadMore" element={<DateReadMore/>}/>
 
-                    <Route path="/" element={<Main></Main>}/>
-                    <Route path="/user/userTest" element={<Test/>}/>
-                    <Route path="/date/dateList" element={<DateList/>}/>
-                    <Route path="/date/dateWrite" element={<DateWrite/>}/>
                     <Route path="/admin" element={<Admin/>}/>
 
                     {/* 소모임 페이지!! */}
