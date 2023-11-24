485e52cddf84fdbf3030cf00435dd15a12457b69
jja6312 <127708710+jja6312@users.noreply.github.com>
2023-11-24 11:10:26 +0900

    수정:리베이스 충돌해결

--- src/main/java/application/FlirdogApplication.java
+++ src/main/java/application/FlirdogApplication.java
@@ -9,15 +9,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "application", "user.*", "product.*", "matching.*", "community.*", "order.*",
-<<<<<<< HEAD
+
 		"payment.*", "admin.*", "s3.*", "mypage.*", "somoim.*" })
-@EntityScan({ "user.bean", "product.bean", "matching.bean", "community.bean", "order.bean", "payment.bean", "somoim.bean"  })
-=======
-		"payment.*", "admin.*", "s3.*", "ai.*" })
-@EntityScan({ "user.bean", "product.bean", "matching.bean", "community.bean", "order.bean", "payment.bean", "ai.bean" })
->>>>>>> a9a871d (임시:1123_1956)
+@EntityScan({ "user.bean", "product.bean", "matching.bean", "community.bean", "order.bean", "payment.bean",
+		"somoim.bean", "ai.bean" })
+
 @EnableJpaRepositories({ "user.repository", "product.repository", "matching.repository", "community.repository",
-		"order.repository", "payment.repository", "admin.repository", "mypage.repository" , "somoim.repository" })
+		"order.repository", "payment.repository", "admin.repository", "mypage.repository", "somoim.repository" })
 @EnableJpaAuditing
 public class FlirdogApplication {
 
