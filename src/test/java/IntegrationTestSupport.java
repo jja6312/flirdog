bccab8eb70601247d5e8984d225a330294d2cbd3
lavi15 <biz.jongin@gmail.com>
2023-12-11 02:03:17 +0900

    33

--- src/test/java/IntegrationTestSupport.java
+++
@@ -1,8 +0,0 @@
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-
-@ActiveProfiles("test")
-@SpringBootTest
-public abstract class IntegrationTestSupport {
-
-}
