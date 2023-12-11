e36b61e9783dbf07b41391125273fb981a2deba4
jja6312 <127708710+jja6312@users.noreply.github.com>
2023-12-11 10:15:36 +0900

    수정:프로젝트 내 이미지 파일 일부 추가

--- src/main/java/matching/bean/MatchingDTO.java
+++ src/main/java/matching/bean/MatchingDTO.java
@@ -22,67 +22,66 @@ import lombok.Setter;
 @Entity
 @Table(name = "matchingTable")
 public class MatchingDTO extends BaseEntity {
-	@Id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-	
-	@NonNull
-	@Column(name="userId", length=50)
+
+    @NonNull
+    @Column(name = "userId", length = 50)
     private Long userId;
-	
-	@NonNull
-	@Column(name="title", length=50)
+
+    @NonNull
+    @Column(name = "title", length = 50)
     private String title;
-	
-	@Column(name="content", length=500)
+
+    @Column(name = "content", length = 500)
     private String content;
-	
-	@Column(name="iamge", length=500)
-	private String image;
-    
-	@Column(name="dogName", length=30)
+
+    @Column(name = "image", length = 500)
+    private String image;
+
+    @Column(name = "dogName", length = 30)
     private String dogName;
-    
-	@Column(name="dogAge", length=20)
+
+    @Column(name = "dogAge", length = 20)
     private String dogAge;
-    
-	@Column(name="dogGender", length=20)
+
+    @Column(name = "dogGender", length = 20)
     private String dogGender;
-    
-	@Column(name="isNeutralized", length=20)
+
+    @Column(name = "isNeutralized", length = 20)
     private Boolean isNeutralized;
-    
-	@Column(name="dogBreed", length=50)
+
+    @Column(name = "dogBreed", length = 50)
     private String dogBreed;
-    
-	@Column(name="date", length=50)
+
+    @Column(name = "date", length = 50)
     private String date;
-	
-	@Column(name="matchingPurpose", length=50)
-	private String matchingPurpose;
-	
-	@Column(name="matchingState", length=50)
-	private String matchingState;
-    
-	@Column(name="matchingAddress", length=200)
+
+    @Column(name = "matchingPurpose", length = 50)
+    private String matchingPurpose;
+
+    @Column(name = "matchingState", length = 50)
+    private String matchingState;
+
+    @Column(name = "matchingAddress", length = 200)
     private String matchingAddress;
-	
-	@Column(name="averageScore", length=50)
-	private String averageScore;
-    
-	@Column(name="communityScore", length=200)
+
+    @Column(name = "averageScore", length = 50)
+    private String averageScore;
+
+    @Column(name = "communityScore", length = 200)
     private int communityScore;
-    
-    
-    @Column(name="hit", length=100)
-    @GeneratedValue(strategy= GenerationType.IDENTITY) //MySQL의 AUTO-INCREMENT를 사용하여 자동으로 시쿼스 적용
+
+    @Column(name = "hit", length = 100)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // MySQL의 AUTO-INCREMENT를 사용하여 자동으로 시쿼스 적용
     private int hit;
-    
+
     @Builder(toBuilder = true)
     public MatchingDTO(Long userId, String title, String content, String dogName, String dogAge, String dogGender,
-                        Boolean isNeutralized, String dogBreed, String date, String matchingState,
-                        String matchingAddress, String matchingPurpose, String image, String averageScore, int communityScore,
-                        int hit) {
+            Boolean isNeutralized, String dogBreed, String date, String matchingState,
+            String matchingAddress, String matchingPurpose, String image, String averageScore, int communityScore,
+            int hit) {
         this.userId = userId;
         this.title = title;
         this.content = content;
@@ -106,7 +105,7 @@ public class MatchingDTO extends BaseEntity {
                 .image(String.join(",", imgs))
                 .build();
     }
-    
+
     public int getHit() {
         return this.hit;
     }
